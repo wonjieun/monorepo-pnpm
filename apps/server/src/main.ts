e422e0b38280b6b2f9bb57bd9c9a8069332a11bc
jieun Won <jieunwon@linecorp.com>
2023-08-17 21:47:10 +0900

    Adding the tRPC server to NestJS

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TrpcRouter } from '@server/trpc/trpc.router';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  app.enableCors();
+  const trpc = app.get(TrpcRouter);
+  trpc.applyMiddleware(app);
 
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- apps/server/src/trpc/trpc.module.ts
+++ apps/server/src/trpc/trpc.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TrpcService } from './trpc.service';
+import { TrpcService } from '@server/trpc/trpc.service';
+import { TrpcRouter } from '@server/trpc/trpc.router';
 
 @Module({
-  providers: [TrpcService],
+  imports: [],
+  controllers: [],
+  providers: [TrpcService, TrpcRouter],
 })
 export class TrpcModule {}
