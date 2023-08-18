3c3eb352f4c082622e77bdec850077845aba7778
jieun Won <jieunwon@linecorp.com>
2023-08-18 22:30:30 +0900

    Remove files

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), TrpcModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
